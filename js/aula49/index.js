c7c562a450d738bb0c7cc2798a45303e0468d764
Gabriel Farias <gabrielf6793@gmail.com>
2025-08-30 10:12:55 -0300

    aulas 48 e 49

---
+++ js/aula49/index.js
@@ -0,0 +1,22 @@
+function falaOi(){
+    console.log('oi')
+
+}
+
+const souUmDado = function(){
+    console.log('sou um dado')
+};
+
+souUmDado()
+
+const funcaoArrow = () => {
+    console.log('sou uma arrow function')
+}
+
+funcaoArrow();
+
+const obj ={
+    falar: function(){
+        console.log('estou falando')
+    }
+}
