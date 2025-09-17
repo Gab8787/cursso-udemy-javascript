e701f50eba263589592fbeb723d0b6ec29b5af0a
Gabriel Farias <gabrielf6793@gmail.com>
2025-09-17 19:50:18 -0300

    aula 66

--- js/aula65/index.js
+++ js/aula65/index.js
@@ -1,8 +1,7 @@
 //filter -> Sempre retorna um array com a mesma quantidade de elementos ou menos 
 
 //Retorne os numeros maiores que 10
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-
+const numeros =[5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 const numerosFiltrados = numeros.filter(valor => valor > 10)
 
 
---
+++ js/aula66/index.js
@@ -0,0 +1,31 @@
+//map
+
+const numeros =[5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+const double = numeros.map(val => val*2)
+console.log(double)
+
+const pessoas =[
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47}
+]
+
+const nomes = pessoas.map(obj => obj.nome)
+//const idades = pessoas.map((obj)=>{
+    //delete obj.nome
+    //return obj.idade
+//})
+
+const comIds = pessoas.map((obj, indice)=>{
+    const newObj = {...obj}
+    newObj.id = indice
+    return newObj
+})
+
+
+console.log(comIds)
+console.log(pessoas)
