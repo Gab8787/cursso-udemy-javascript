50ec00cb2286a64c6d43a83cfb86060e3818a7cb
Gabriel Farias <gabrielf6793@gmail.com>
2025-09-17 17:53:49 -0300

    aula 63

---
+++ js/aula63/index.js
@@ -0,0 +1,7 @@
+const nomes = ['maria', 'joao', 'eduardo', 'gabriel', 'julia']
+//push
+nomes.splice(0, 0, 'luiz')
+console.log(nomes)
+//nomes.splice(indice, delete, elem1, elem2, elem3)
+//pop
+//const removidos = nomes.splice(-1, 1)
