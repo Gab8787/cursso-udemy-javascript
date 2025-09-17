f6d93e3e1faaa06c4acfd6cce7fe9bb3623c6237
Gabriel Farias <gabrielf6793@gmail.com>
2025-09-17 17:03:16 -0300

    aula 60, 61 e 62

---
+++ js/aula62/index.js
@@ -0,0 +1,10 @@
+//const nomes = ['eduardo', 'maria', 'joana', 'wallace', '///rosana']
+//const novo = nomes.slice(0, -1)
+//console.log(novo)
+
+//const nome = 'luiz, otavio, miranda'
+//const nomes = nome.split(' ')
+const nomes = ['luiz', 'otavio', 'miranda'
+]
+const nome = nomes.join(', ')
+console.log(nomes)
