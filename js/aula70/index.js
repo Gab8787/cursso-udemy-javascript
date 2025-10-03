3998976e1f047e8205fcc50d18ed5382530afe64
Gabriel Farias <gabrielf6793@gmail.com>
2025-10-03 18:52:02 -0300

    aula 70

---
+++ js/aula70/index.js
@@ -0,0 +1,12 @@
+//factory function
+//constructor function
+
+function Pessoa(nome, sobrenome){
+    this.nome = nome
+    this.sobrenome = sobrenome 
+}
+const p1 = new Pessoa('Luiz', 'Miranda')
+const p2 = new Pessoa('Maria', 'Miranda')
+console.log(p1)
+console.log(p2)
+
