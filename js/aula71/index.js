0eab9392cb99f519f4f20b6228c69d0763b335bb
Gabriel Farias <gabrielf6793@gmail.com>
2025-10-07 18:20:15 -0300

    aula 71

---
+++ js/aula71/index.js
@@ -0,0 +1,19 @@
+//define property - define properties
+function Produto (nome, preco, estoque){
+    this.nome = nome;
+    this.preco = preco;
+    this.estoque = estoque;
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        value: estoque,
+        writable: true,
+        configurable: true
+
+
+    });
+
+}
+
+const p1 = new Produto('Camiseta', 20, 3);
+
+console.log(Object.keys(p1))
