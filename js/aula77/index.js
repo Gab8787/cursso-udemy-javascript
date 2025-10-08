a9d360f58835f874bba4b74c7213e020c4d8a7c4
Gabriel Farias <gabrielf6793@gmail.com>
2025-10-08 18:50:05 -0300

    aula 77

---
+++ js/aula77/index.js
@@ -0,0 +1,6 @@
+//validaçao cpf
+
+let cpf = '705.484.450-52'
+let cpfLimpo = cpf.replace(/\D+/g, '')
+cpfArray = Array.from(cpfLimpo);
+console.log(cpfArray.reduce(ac, val) => ac + Number (val, 0))
