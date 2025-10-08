61e1179ca9f6cb914f33b78dd210964928460574
Gabriel Farias <gabrielf6793@gmail.com>
2025-10-08 19:30:13 -0300

    aula 77

--- js/aula77/index.js
+++ js/aula77/index.js
@@ -1,6 +1,41 @@
-//validaçao cpf
+function ValidaCpf(cpfEnviado){
+Object.defineProperty(this, 'cpfLimpo', {
+    get: 
+        function(){
+            return cpfEnviado.replace(/\D+/g, '')
+        }
+    
+})
+}
 
-let cpf = '705.484.450-52'
-let cpfLimpo = cpf.replace(/\D+/g, '')
-cpfArray = Array.from(cpfLimpo);
-console.log(cpfArray.reduce(ac, val) => ac + Number (val, 0))
+ValidaCpf.prototype.valida = function(){
+if(typeof this.cpfLimpo === "undefined") return false;
+if(this.cpfLimpo.length !== 11)return false;
+if(this.iSequencia())return false;
+const cpfParcial = this.cpfLimpo.slice(0, -2)
+const digito1 = this.criaDigito(cpfParcial)
+const digito2 = this.criaDigito(cpfParcial + digito1)
+const novoCpf = cpfParcial + digito1 + digito2
+
+    return novoCpf === this.cpfLimpo
+}
+
+ValidaCpf.prototype.criaDigito = function(cpfParcial){
+const cpfArray = Array.from(cpfParcial);
+let regresssivo = cpfArray.length + 1
+const total = cpfArray.reduce((ac, val) => {
+    ac += (regresssivo * Number(val))
+
+regresssivo--
+return ac
+}, 0)
+const digito = 11 - (total % 11)
+return digito > 9 ?  '0': String(digito)
+}
+
+ValidaCpf.prototype.iSequencia = function(){
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+    return sequencia === this.cpfLimpo
+}
+const cpf = new ValidaCpf('111.111.111-11 ')
+console.log(cpf.valida())
