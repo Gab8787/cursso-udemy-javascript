77ce7246e32d6aa412e5b629f7d003282ed87af2
Gabriel Farias <gabrielf6793@gmail.com>
2025-10-20 19:34:16 -0300

    aula 84

--- js/aula84/index.js
+++ js/aula84/index.js
@@ -1,27 +1,55 @@
-class ControleRemoto{
-    constructor(tv){
-        this.tv = tv
-        this.volume = 0
-    }
-    //Metodo de instância
-
-    aumentarVolume(){
-        this.volume+= 2
-    }
-    //Metodo de instância
-    diminuirVolume(){
-        this.volume-= 2
-    }
-    
-    static trocaPilha(){
-        console.log('okei, vou trocar')
+class ValidaCPF{
+    constructor(cpfEnviado){
+        Object.defineProperty(this, 'cpfLimpo', {
+            value: cpfEnviado.replace(/\D+/g, '')
+        })
 
     }
+
+    éSequencia(){
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
+    }
+
+    geraNovoCpf(){
+        const cpfSemdigitos= this.cpfLimpo.slice(0, -2)
+        const digito1 = this.geraDigito(cpfSemdigitos)
+        const digito2 = this.geraDigito(cpfSemdigitos + digito1)
+        
+        this.novoCPF = cpfSemdigitos  + digito1 + digito2
+    }
+geraDigito(cpfParcial){
+    let total = 0;
+    let reverso = cpfParcial.length + 1;
+
+    for(let numero of cpfParcial){
+        total += reverso * Number(numero);
+        reverso--;
+    }
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
 }
 
-const c1 = new ControleRemoto('lg')
 
-c1.aumentarVolume()
-c1.aumentarVolume()
-ControleRemoto.trocaPilha()
-console.log(c1)
+  valida() {
+    if(!this.cpfLimpo) return false;
+    if(typeof this.cpfLimpo !== 'string') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.éSequencia()) return false;
+
+    this.geraNovoCpf(); 
+
+    console.log(this.novoCPF);
+    return this.novoCPF === this.cpfLimpo;
+}
+
+}
+
+const validacpf = new ValidaCPF('070.987.720-03')
+
+if(validacpf.valida === true){
+    console.log('CPF valido')
+}else{
+    console.log('cpf invalido')
+
+}
--- js/modelo/assets/js/main.js
+++ js/modelo/assets/js/main.js
@@ -1,26 +1,5 @@
-class JogoTerror{
-    constructor(nome){
-        this.nome = nome
-        this.idade = 16
-
-    }
-
-    verificaIdade(valor){
-        if(valor < this.idade){
-            console.log('nao tem idade suficiente para jogar')
-        }else{
-            console.log('boa jogatina, voce tem a idade suficiente ')
-        }
+class Game{
+    constructor(name, type, age,){
 
     }
 }
-
-class FilmeTerror extends JogoTerror{}
-
-const p1 = new JogoTerror('dead')
-const p2 = new FilmeTerror('jaison')
-
-p1.verificaIdade(15)
-p2.verificaIdade(16)
-
-console.log(p2)
